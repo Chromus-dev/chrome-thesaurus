3b8a87dd041e06c32da5e8d010b5112681f1986a
Chromus_ <Chromus-dev@users.noreply.github.com>
2022-12-02 08:02:06 -0800

    basic context menu

--- background.js
+++ background.js
@@ -1,1 +1,20 @@
 'use strict';
+
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create(
+		{
+			id: 'showSynonyms',
+			title: 'Show Synonyms',
+			contexts: ['selection'],
+			type: 'normal',
+			onclick: (clickData) => {
+				console.log(clickData.selectionText);
+			},
+		},
+		() => console.log(chrome.runtime.lastError)
+	);
+});
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+	console.log(clickData.selectionText);
+});
