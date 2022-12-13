d03959a57f20b7d132a4e8621680b872bb227ac8
Chromus <xlchannel6@gmail.com>
2022-12-13 14:01:27 -0800

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,9 @@
 // https://stackoverflow.com/questions/33217344/chrome-extension-contextmenus-create-title-property-validator
 document.addEventListener('selectionchange', (e) => {
-	chrome.runtime.sendMessage({ selection: getSelection().toString().trim() });
+	chrome.runtime.sendMessage({ name: 'selectionUpdated', data: getSelection().toString().trim() });
 });
+chrome.runtime.onMessage.addListener(async (msg) => {
+	if(msg.name === 'copyToClipboard'){
+		await navigator.clipboard.writeText(msg.data);
+	}
+})
